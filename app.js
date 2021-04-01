80fb93f29e6f5fc887d3045fafdc9fe2c44166ca
Arpit Bhalla <arpitbhalla2001@gmail.com>
2021-04-01 20:32:25 +0530

    Added

--- app.js
+++ app.js
@@ -72,8 +72,6 @@ passport.use(
       db.query(
         "select * from users where email = '" + email + "'",
         function (err, rows) {
-          console.log(rows);
-          console.log("above row object");
           if (err) return done(err);
           if (rows.length) {
             return done(
@@ -125,18 +123,15 @@ passport.use(
               false,
               req.flash("loginMessage", "No user found.")
             );
-            // req.flash is the way to set flashdata using connect-flash
           }
 
-          // if the user is found but the password is wrong
           if (!(rows[0].password == password))
             return done(
               null,
               false,
               req.flash("loginMessage", "Oops! Wrong password.")
-            ); // create the loginMessage and save it to session as flashdata
+            );
 
-          // all is well, return successful user
           return done(null, rows[0]);
         }
       );
